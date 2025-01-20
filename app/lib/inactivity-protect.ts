c74168d59174dfe852db4a6c59724fd86f60c081
khine-dev <khine.dev.9417@gmail.com>
2025-01-21 05:15:59 +0700

    inactivity protect

---
+++ app/lib/inactivity-protect.ts
@@ -0,0 +1,16 @@
+export default async function inactivity_protect() {
+    try {
+        const auth_service = process.env.AUTH_SERVICE || '';
+        const browse_service = process.env.BROWSE_SERVICE || '';
+
+        const [ res_1 , res_2 ] = await Promise.all([
+            fetch(auth_service), 
+            fetch(browse_service)
+        ]);
+        console.log('the responses', res_1, res_2);
+        console.log('inactivity protection successfully');
+    } catch (e) {
+       console.log(e);
+       console.log('error in inactivity protection'); 
+    }
+}
