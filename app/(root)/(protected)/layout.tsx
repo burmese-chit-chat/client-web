b0fc8dbb7fe151d07c43c982c2db1165d5a0e1a8
khine-dev <khine.dev.9417@gmail.com>
2025-01-21 02:48:53 +0700

    browse page fix

--- app/(root)/(protected)/layout.tsx
+++ app/(root)/(protected)/layout.tsx
@@ -1,11 +1,11 @@
 import get_me from "@/app/lib/get-me";
 import IUser from "@/app/types/IUser";
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 import React from "react";
 
 export default async function layout({ children }: { children: React.ReactNode }) {
     const user : IUser | null = await get_me();
     console.log('user from protected layout', user);
-    if(!user) redirect("/auth/register");
+    // if(!user) redirect("/auth/register");
     return <>{children}</>;
 }
