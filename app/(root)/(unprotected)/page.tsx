cfdfd62a0d295097659f8cd260962c1eb97ab03c
khine-dev <khine.dev.9417@gmail.com>
2025-01-21 03:06:41 +0700

    please work

--- app/(root)/(unprotected)/page.tsx
+++ app/(root)/(unprotected)/page.tsx
@@ -1,11 +1,39 @@
+import { AvatarFallback } from "@/components/ui/avatar";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import Link from "next/link";
 
 export default async function Home() {
+    const image_1 = "https://i.postimg.cc/85M4DKR5/6080355312726032716.jpg";
+    const image_2 = "https://i.postimg.cc/VLwnCfcK/6084494651882064070.jpg";
     return (
         <>
             <div className="font-bold uppercase">Home Page</div>
             <Separator className="my-3"></Separator>
-            <div>I do not know what to put in the home page yet so, here is a gift for my dear testers.</div>
+            <Card className="">
+                <CardHeader>
+                    <CardTitle className="text-center">
+                        I do not know what to put in the home page yet so, here is a gift for my dear testers.
+                        <br/>
+                        Lovely Khine !!
+                    </CardTitle>
+                </CardHeader>
+                <CardContent className="flex justify-center gap-3 h-[500px]">
+                    <Link href={image_1} target="_blank">
+                        <Avatar className="h-full">
+                            <AvatarImage className="w-full h-full" src={image_1} alt="my pic"></AvatarImage>
+                            <AvatarFallback>sorry!! :(</AvatarFallback>
+                        </Avatar>
+                    </Link>
+                    <Link href={image_2} target="_blank">
+                        <Avatar className="h-full">
+                            <AvatarImage className="w-full h-full" src={image_2} alt="my pic"></AvatarImage>
+                            <AvatarFallback>sorry!! :(</AvatarFallback>
+                        </Avatar>
+                    </Link>
+                </CardContent>
+            </Card>
         </>
     );
 }
