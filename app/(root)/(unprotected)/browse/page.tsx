3af83169587742e4490ea2564ff58afd35a5f8bf
khine-dev <khine.dev.9417@gmail.com>
2025-02-13 06:31:21 +0700

    active status

--- app/(root)/(unprotected)/browse/page.tsx
+++ app/(root)/(unprotected)/browse/page.tsx
@@ -7,37 +7,39 @@ import PaginationComponent from "@/app/components/pagination";
 import SearchForm from "./components/search-form";
 import IUser from "@/app/types/IUser";
 
-type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
-export default async function BrowsePage({ searchParams } : { searchParams: SearchParams }) {
+export default async function BrowsePage({ searchParams }: { searchParams: SearchParams }) {
     const currentPage = Number((await searchParams).page || 1);
     const current_search_keyword = String((await searchParams).search || "");
     // console.log("current page", currentPage);
     const { users, pagination } = await browse_all_users(currentPage, current_search_keyword);
+    console.log('the users', users);
     return (
         <>
             <h1 className="font-bold uppercase mb-4">Our lovely users</h1>
             <div>
-                <SearchForm page={currentPage} keyword={current_search_keyword}/>
+                <SearchForm page={currentPage} keyword={current_search_keyword} />
             </div>
             {!users.length && <div className="opacity-80 my-4">No users found...</div>}
             <div>
                 {!!users.length && (
                     <div className="mt-4 space-y-4">
                         {!!users.length &&
-                            users.map((item : IUser, i : number) => (
-                                <Link href={`browse/users/${item.username}`} className="block" key={item._id}>
-                                    <GeneralInfo _id={item._id} username={item.username} name={item?.name || item.username} profile={item?.profile?.secure_url || ""} gender={item?.gender} age={item?.age} region={item?.region} open_profile={false} />
-                                    {users.length !== i + 1 && <Separator className="mt-2"></Separator>}
-                                </Link>
+                            users.map((item: IUser, i: number) => (
+                                <div key={item._id}>
+                                    <Link href={`browse/users/${item.username}`} className="flex items-center justify-between pe-3">
+                                        <GeneralInfo _id={item._id} username={item.username} name={item?.name || item.username} profile={item?.profile?.secure_url || ""} gender={item?.gender} age={item?.age} region={item?.region} open_profile={false} />
+                                        { item.is_active && <div className="w-2 h-2 rounded-full bg-green-500"></div> }
+                                    </Link>
+                                    {users.length !== i + 1 && <Separator className="mt-4"></Separator>}
+                                </div>
                             ))}
                     </div>
                 )}
             </div>
             <Separator className="my-4" />
-            { !!users.length && (
-                <PaginationComponent search_keyword={current_search_keyword} totalPages={pagination.total} currentPage={currentPage} basePath="/browse" maxDisplayed={2} />
-            ) }
+            {!!users.length && <PaginationComponent search_keyword={current_search_keyword} totalPages={pagination.total} currentPage={currentPage} basePath="/browse" maxDisplayed={2} />}
         </>
     );
 }
