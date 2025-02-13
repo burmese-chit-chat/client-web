14c1dd0e0f88cdf0bfa528b5b2b1296836a956b8
khine-dev <khine.dev.9417@gmail.com>
2025-02-14 06:53:41 +0700

    home page fix

--- app/(root)/(unprotected)/page.tsx
+++ app/(root)/(unprotected)/page.tsx
@@ -1,31 +1,31 @@
-import { AvatarFallback } from "@/components/ui/avatar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
-import Link from "next/link";
+import CopyToClipboard from "../components/copy-to-clipboard";
 
 export default async function Home() {
-    // const image_1 = "https://i.postimg.cc/85M4DKR5/6080355312726032716.jpg";
-    const image_2 = "https://i.postimg.cc/VLwnCfcK/6084494651882064070.jpg";
+    const gmail = "khine.dev.9417@gmail.com";
     return (
         <>
-            <div className="font-bold uppercase">Home Page</div>
+            <div className="font-bold uppercase">Home</div>
             <Separator className="my-3"></Separator>
             <Card className="mb-24">
                 <CardHeader>
-                    <CardTitle className="text-center">
-                        I do not know what to put in the home page yet so, here is a gift for my dear testers.
-                        <br/>
-                        Lovely Khine !!
+                    <CardTitle>
+                        ANNOUNCEMENT!!
                     </CardTitle>
                 </CardHeader>
-                <CardContent className="flex justify-center gap-3 h-[500px]">
-                    <Link href={image_2} target="_blank">
-                        <Avatar className="h-full">
-                            <AvatarImage className="w-full h-full" src={image_2} alt="my pic"></AvatarImage>
-                            <AvatarFallback className="min-w-[200px]">loading...</AvatarFallback>
-                        </Avatar>
-                    </Link>
+                <CardContent className="space-y-3">
+                    <p>
+                        - This is the early stage of the platform and there can be many bugs. 
+                    </p>
+                    <Separator />
+                    <p>
+                        - You can report the bugs and some inconveniences to <CopyToClipboard text={gmail}/>.
+                    </p>
+                    <Separator />
+                    <p className="">
+                        - <span className="font-bold text-lg">Mobile application will be coming soon !!</span>
+                    </p>
                 </CardContent>
 
             </Card>
