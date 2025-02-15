85ddba4a02479666dd5b62dc1735a667f0f82a48
khine-dev <khine.dev.9417@gmail.com>
2025-02-15 07:18:51 +0700

    ad test

--- app/(root)/(unprotected)/page.tsx
+++ app/(root)/(unprotected)/page.tsx
@@ -10,24 +10,24 @@ export default async function Home() {
             <Separator className="my-3"></Separator>
             <Card className="mb-24">
                 <CardHeader>
-                    <CardTitle>
-                        ANNOUNCEMENT!!
-                    </CardTitle>
+                    <CardTitle>ANNOUNCEMENT!!</CardTitle>
                 </CardHeader>
                 <CardContent className="space-y-3">
-                    <p>
-                        - This is the early stage of the platform and there can be many bugs. 
-                    </p>
+                    <p>- This is the early stage of the platform and there can be many bugs.</p>
                     <Separator />
                     <p>
-                        - You can report the bugs and some inconveniences to <CopyToClipboard text={gmail}/>.
+                        - You can report the bugs and some inconveniences to <CopyToClipboard text={gmail} />.
                     </p>
                     <Separator />
                     <p className="">
                         - <span className="font-bold text-lg">Mobile application will be coming soon !!</span>
                     </p>
                 </CardContent>
-
+            </Card>
+            <Card>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7511160783457466" crossOrigin="anonymous"></script>
+                <ins className="adsbygoogle block" data-ad-client="ca-pub-7511160783457466" data-ad-slot="1866067277" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
             </Card>
         </>
     );
