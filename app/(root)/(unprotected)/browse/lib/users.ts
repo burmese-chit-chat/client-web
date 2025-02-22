fc23207301ac34616b2023d3b4ae6d29d0b6ef3a
khine-dev <khine.dev.9417@gmail.com>
2025-02-23 02:18:01 +0700

    gender filter

--- app/(root)/(unprotected)/browse/lib/users.ts
+++ app/(root)/(unprotected)/browse/lib/users.ts
@@ -1,12 +1,12 @@
 import IUser from "@/app/types/IUser";
+import { EnumGender } from "@/app/types/TGenders";
 
-export async function browse_all_users (page : number, search : string) : Promise<{users : IUser[], pagination : { total : number, page : number}}> {
+export async function browse_all_users (page : number, search : string, gender : EnumGender | null) : Promise<{users : IUser[], pagination : { total : number, page : number}}> {
     try {
         const base_url = process.env.BASE_URL;
-        // console.log("from browse_all_users", page, search);
-        const response = await fetch(`${base_url}/api/browse/users?page=${page}&search=${search}`, { next : { revalidate : 0 }});
+        const url = gender ? `${base_url}/api/browse/users?page=${page}&search=${search}&gender=${gender}` : `${base_url}/api/browse/users?page=${page}&search=${search}`;
+        const response = await fetch(url);
         const json_response = await response.json();
-        console.log("from browse all users", json_response);
         const users = json_response.data;
         const pagination = json_response.pagination;
         if(json_response.status !== 200) throw new Error('users not found');
